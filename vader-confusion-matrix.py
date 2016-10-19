46bd1b4989fe02eda134dd4ac43d594e8914ecf6
Nicklaus Glyder <nglyder@user001.palmetto.clemson.edu>
2016-10-19 15:41:07 -0400

    Tweaking classifier

--- vader-confusion-matrix.py
+++ vader-confusion-matrix.py
@@ -2,16 +2,17 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from enum import IntEnum
 import json
 import codecs
+import re
+
 # For classification
 class Positivity(IntEnum):
     positive = 1
     negative = -1
-    neutral = 0
 
 file_name = "CompiledTweets.txt"
 
 # Threshold for classifier, > 0.25 is positive, < -0.25 is negative, else neutral
-THRESHOLD = 0.15
+THRESHOLD = 0.50
 
 hand_coded_tweets = []
 hand_coded_json = None
@@ -29,57 +30,46 @@ with codecs.open(file_name, "r", encoding='utf-8', errors='ignore') as CompiledT
 		hand_coded_tweets.append(tweet)
 
 	hand_coded_json  = json.dumps(hand_coded_tweets)
+print("Total Tweets {0}".format(len(hand_coded_tweets)))
 
 confusion_matrix = {
 	'pos_pos': 0,
 	'pos_neg': 0,
-	'pos_neu': 0,
 	#############
 	'neg_pos': 0,
-	'neg_neg': 0,
-	'neg_neu': 0,
-	#############
-	'neu_pos': 0,
-	'neu_neg': 0,
-	'neu_neu': 0
+	'neg_neg': 0
 }
 
 f = open('ClassifiedTweets','w')
 
 vaderAnalyzer = SentimentIntensityAnalyzer()
+classified = 0
 for tweet in hand_coded_tweets:
 	scores = vaderAnalyzer.polarity_scores(tweet['text'])
 	if scores['compound'] >= THRESHOLD:
 		tweet['sentiment'] = Positivity.positive
+		classified += 1
 	elif scores['compound'] <= -THRESHOLD:
 		tweet['sentiment'] = Positivity.negative
+		classified += 1
 	else:
-		tweet['sentiment'] = Positivity.neutral
-
+		print(scores)
+		hand_coded_tweets.remove(tweet)
+		continue
 
 	if int(tweet['code']) == 1:
 		if tweet['sentiment'] == Positivity.positive:
 			confusion_matrix['pos_pos'] += 1				
 		elif tweet['sentiment'] == Positivity.negative:
 			confusion_matrix['pos_neg'] += 1				
-		else:
-			confusion_matrix['pos_neu'] += 1				
 	elif int(tweet['code']) == -1:
 		if tweet['sentiment'] == Positivity.positive:
 			confusion_matrix['neg_pos'] += 1				
 		elif tweet['sentiment'] == Positivity.negative:
-			confusion_matrix['neg_neg'] += 1				
-		else:
-			confusion_matrix['neg_neu'] += 1				
-	else:
-		if tweet['sentiment'] == Positivity.positive:
-			confusion_matrix['neu_pos'] += 1				
-		elif tweet['sentiment'] == Positivity.negative:
-			confusion_matrix['neu_neg'] += 1				
-		else:
-			confusion_matrix['neu_neu'] += 1				
+			confusion_matrix['neg_neg'] += 1
 	print(tweet, file=f)
-print("Total Tweets Analyzed: {0}\n".format(len(hand_coded_tweets)))
+
+print("Total Tweets Analyzed: {0}\n".format(classified))
 
 for key, value in sorted(confusion_matrix.items()): # Note the () after items!
     print(key, value)
